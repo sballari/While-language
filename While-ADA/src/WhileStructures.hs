c1ca71ed6cf1813c00b735aa06ae19ccd6809053
sballari <ballarinsimone@gmail.com>
2018-12-19 17:29:07 +0100

    created module

--- While-ADA/src/WhileStructures.hs
+++ While-ADA/src/WhileStructures.hs
@@ -1,23 +1,25 @@
-type Name = String
+module WhileStructures where 
 
-data AExpr = 
-    Sum AExpr AExpr  
-    | Mul AExpr AExpr 
-    | Min AExpr AExpr 
-    | Var Name 
-    | Num Int -- integer numeric values (Z)
+    type Name = String
 
-data BExpr = 
-    True 
-    | False 
-    | Eq AExpr AExpr
-    | LessEq AExpr AExpr
-    | Neg BExpr
-    | And BExpr BExpr
+    data AExpr = 
+        Sum AExpr AExpr  
+        | Mul AExpr AExpr 
+        | Min AExpr AExpr 
+        | Var Name 
+        | Num Int -- integer numeric values (Z)
 
-data Stm = 
-    Assign Name AExpr
-    |Skip
-    |Comp Stm Stm
-    |Cond BExpr Stm Stm -- if b then S1 else S2     
-    |While BExpr Stm -- while b S
+    data BExpr = 
+        True 
+        | False 
+        | Eq AExpr AExpr
+        | LessEq AExpr AExpr
+        | Neg BExpr
+        | And BExpr BExpr
+
+    data Stm = 
+        Assign Name AExpr
+        |Skip
+        |Comp Stm Stm
+        |Cond BExpr Stm Stm -- if b then S1 else S2     
+        |While BExpr Stm -- while b S
