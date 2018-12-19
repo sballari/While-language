c28b1b8d4a6926e6e3f6ee495f95d24013924d51
sballari <ballarinsimone@gmail.com>
2018-12-19 17:26:25 +0100

    added main data types for parsing

---
+++ While-ADA/src/WhileStructures.hs
@@ -0,0 +1,23 @@
+type Name = String
+
+data AExpr = 
+    Sum AExpr AExpr  
+    | Mul AExpr AExpr 
+    | Min AExpr AExpr 
+    | Var Name 
+    | Num Int -- integer numeric values (Z)
+
+data BExpr = 
+    True 
+    | False 
+    | Eq AExpr AExpr
+    | LessEq AExpr AExpr
+    | Neg BExpr
+    | And BExpr BExpr
+
+data Stm = 
+    Assign Name AExpr
+    |Skip
+    |Comp Stm Stm
+    |Cond BExpr Stm Stm -- if b then S1 else S2     
+    |While BExpr Stm -- while b S
