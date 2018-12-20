69a0f64a208c8eae0f1e2878fa0e42eff8462396
sballari <ballarinsimone@gmail.com>
2018-12-20 18:14:50 +0100

    added new tests

--- While-ADA/test/ParserTest.hs
+++ While-ADA/test/ParserTest.hs
@@ -4,7 +4,10 @@ module ParserTest (tests) where
     import Test.Tasty
     import Test.Tasty.HUnit
 
-    tests = [testAExp, testBExpr]
+    tests = [testAExp, testBExpr, testSComp, testMComp, testStm, composedIf, composedIf2, oneStm]
+
+    resultP :: Parser a -> String -> a
+    resultP p i = (\[(x,y)]->x) (parse p i)
 
     testAExp  = testCase "parse AExp" (assertEqual "" expected result)
         where 
@@ -13,11 +16,11 @@ module ParserTest (tests) where
                  Mul (Var "x") (Mul (Num 2) (Var "y")),
                  Mul (Mul (Var "x") (Num 2)) (Var "y")]
             result =
-                [(\[(x,y)]->x) (parse parseAExpr "x+2*y"),
-                 (\[(x,y)]->x) (parse parseAExpr "x*(2*y)"),
-                 (\[(x,y)]->x) (parse parseAExpr "(x*2)*y")]
+                [resultP parseAExpr "x+2*y",
+                 resultP parseAExpr "x*(2*y)",
+                 resultP parseAExpr "(x*2)*y"]
     
-    testBExpr = testCase "parseBExpr" (assertEqual "" expected result)
+    testBExpr = testCase "parse BExpr" (assertEqual "" expected result)
         where 
             expected = 
                 [LessEq (Min (Num 4) (Num 3)) (Num 5),
@@ -27,11 +30,51 @@ module ParserTest (tests) where
                  And (Neg (Eq (Num 4)(Num 8))) (WS.False) 
                 ]
             result =
-                [(\[(x,y)]->x) (parse parseBExpr "(4-3)<=5"),
-                 (\[(x,y)]->x) (parse parseBExpr "!true & (8<=2)"),
-                 (\[(x,y)]->x) (parse parseBExpr "!true & false"),
-                 (\[(x,y)]->x) (parse parseBExpr "!(true & false)"),
-                 (\[(x,y)]->x) (parse parseBExpr "!((4=8)) & false)")
+                [resultP parseBExpr "(4-3)<=5",
+                 resultP parseBExpr "!true & (8<=2)",
+                 resultP parseBExpr "!true & false",
+                 resultP parseBExpr "!(true & false)",
+                 resultP parseBExpr "!((4=8)) & false)"
                 ]
 
+    testStm = testCase "parse a Stm" (assertEqual "" expected result)
+        where 
+            expected = 
+                [Skip,
+                 Assign "x" (Num 93),
+                 Cond (LessEq (Num 3) (Num 33)) Skip (Assign "x" (Num 1)),
+                 While WS.True Skip,
+                 Skip]
+            result =
+                [resultP parseStm "skip",
+                 resultP parseStm "x:=93",
+                 resultP parseStm "if 3<=33 then skip else x:=1",
+                 resultP parseStm "while true do skip",
+                 resultP parseStm "(skip)"]
+
+    testSComp = testCase "parse simple composition" (assertEqual "" expected result)
+        where 
+            expected = [Comp Skip Skip]
+            result = [resultP parseStms "skip; skip"]
+
+    testMComp = testCase "parse multiple composition" (assertEqual "" expected result)
+        where 
+            expected = [Comp Skip (Comp Skip Skip)]
+            result = [resultP parseStms "skip; skip; skip"]
+
+    composedIf = testCase "[if] composed if" (assertEqual "" expected result)
+        where 
+            expected = Comp (Cond (Eq (Num 1) (Num 1)) Skip (Assign "x" (Num 1))) (Assign "y" (Num 2))
+            result = resultP parseStms "if 1=1 then skip else x:=1;y:=2"
     
+    
+    
+    composedIf2 = testCase "[if] composed if 2" (assertEqual "" expected result)
+        where 
+            result = resultP parseStms "if 1=1 then (x:=1;y:=2) else (x:=1;y:=2)"
+            expected = Cond (Eq (Num 1) (Num 1)) (Comp (Assign "x" (Num 1)) (Assign "y" (Num 2))) (Comp (Assign "x" (Num 1)) (Assign "y" (Num 2)))
+
+    oneStm = testCase "x:=3" (assertEqual "" expected result)
+        where 
+            result = resultP parseStms "x:=3"
+            expected = Assign "x" (Num 3)
