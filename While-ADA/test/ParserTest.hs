a2383b849d7f80254bd4d4ee3be116587c3bdcce
sballari <ballarinsimone@gmail.com>
2018-12-20 09:52:45 +0100

    added tests for BExpr

--- While-ADA/test/ParserTest.hs
+++ While-ADA/test/ParserTest.hs
@@ -1,10 +1,10 @@
 module ParserTest (tests) where
     import WhileParser
-    import WhileStructures
+    import WhileStructures as WS
     import Test.Tasty
     import Test.Tasty.HUnit
 
-    tests = [testAExp]
+    tests = [testAExp, testBExpr]
 
     testAExp  = testCase "parse AExp" (assertEqual "" expected result)
         where 
@@ -16,5 +16,22 @@ module ParserTest (tests) where
                 [(\[(x,y)]->x) (parse parseAExpr "x+2*y"),
                  (\[(x,y)]->x) (parse parseAExpr "x*(2*y)"),
                  (\[(x,y)]->x) (parse parseAExpr "(x*2)*y")]
+    
+    testBExpr = testCase "parseBExpr" (assertEqual "" expected result)
+        where 
+            expected = 
+                [LessEq (Min (Num 4) (Num 3)) (Num 5),
+                 And (Neg WS.True) (LessEq (Num 8) (Num 2)),
+                 And (Neg WS.True) WS.False,
+                 Neg (And WS.True WS.False),
+                 And (Neg (Eq (Num 4)(Num 8))) (WS.False) 
+                ]
+            result =
+                [(\[(x,y)]->x) (parse parseBExpr "(4-3)<=5"),
+                 (\[(x,y)]->x) (parse parseBExpr "!true & (8<=2)"),
+                 (\[(x,y)]->x) (parse parseBExpr "!true & false"),
+                 (\[(x,y)]->x) (parse parseBExpr "!(true & false)"),
+                 (\[(x,y)]->x) (parse parseBExpr "!((4=8)) & false)")
+                ]
 
     
