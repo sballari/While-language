e00b3591aef64ce9df0adde879d03f264d3cdd19
sballari <ballarinsimone@gmail.com>
2018-12-24 12:10:58 +0100

    new test

--- While-ADA/test/Spec.hs
+++ While-ADA/test/Spec.hs
@@ -4,11 +4,11 @@ import Test.Tasty.HUnit
 
 import ParserTest
 import SignDomainTest
+import AbsStateTest
 
 main :: IO ()
 main = do
   defaultMain (testGroup "Library tests" tests)
     where
-      tests = ParserTest.tests ++ SignDomainTest.tests  
-
+      tests = ParserTest.tests ++ SignDomainTest.tests ++ AbsStateTest.tests
 
