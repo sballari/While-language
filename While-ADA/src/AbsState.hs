129fac8b6ceee8b2972aed89dc0a2ad1e096e575
sballari <ballarinsimone@gmail.com>
2018-12-29 15:57:25 +0100

    added on absstate bottom and top

--- While-ADA/src/AbsState.hs
+++ While-ADA/src/AbsState.hs
@@ -1,24 +1,55 @@
 module AbsState where  
-    import AbsDomain
-    import Data.List
+  import AbsDomain as AD
+  import Data.List
 
-    type VarName = String
+  type VarName = String
 
-    type AbsState a =  [(VarName, a)]
-    -- a should be the abstract domain
-    -- haskell doesn't allow type constraint in data type
+  data AbsState a = S [(VarName, a)] | Top | Bottom deriving (Show, Eq)
+  -- a should be the abstract domain
+  -- haskell doesn't allow type constraint in data type
 
-    alter :: AbsState a -> String -> a -> AbsState a
-    alter [] name v = [(name, v)]
-    alter (x:xs) name v = if (fst x) == name then (name,v):xs 
-                          else x:(alter xs name v)
-     
-    lookUp :: (UndefSup a) => AbsState a -> String -> a  
-    lookUp s name = case lookup name s of
-                        Nothing -> undef
-                        Just x -> x 
 
-    class UndefSup a where
-      undef :: a 
-    
-    
+  
+  -- instance Functor AbsState where
+  --   -- fmap :: (a->b) -> AbsState a -> AbsState b
+  --   fmap f as = case as of 
+  --                 S xs -> S [(n,f v)|(n,v)<-xs] 
+  --                 AbsState.Top -> AbsState.Top
+  --                 AbsState.Bottom -> AbsState.Bottom
+  -- instance Applicative AbsState where
+  --   -- pure :: a -> AbsState a
+  --   pure a = S ["", a]
+  --   -- <*> :: AbsState (a->b) -> AbsState a -> AbsState b
+  --   f <*> a = 
+
+
+  alter :: AbsState a -> String -> a -> AbsState a
+  alter AbsState.Bottom name v = AbsState.Bottom
+  alter AbsState.Top name v = AbsState.Top
+  alter (S[]) name v = S [(name, v)]
+  alter (S(x:xs)) name v = if (fst x)==name then S ((name,v):xs) 
+                            else 
+                              case (alter (S xs) name v) of 
+                                S xs -> S(x:xs)
+                                AbsState.Top -> AbsState.Top
+                                AbsState.Bottom -> AbsState.Bottom
+
+                            
+                          
+  lookUp :: (UndefSup a) => AbsState a -> String -> a  
+  lookUp AbsState.Bottom name = undef
+  lookUp AbsState.Top name = undef 
+  lookUp (S xs) name = case lookup name xs of
+                      Nothing -> undef
+                      Just x -> x 
+
+  class UndefSup a where
+    undef :: a 
+  
+  -- component wise extensions
+  instance Eq a => AbsDomain (AbsState a) where 
+    (<=) x y = True
+    union x y = x
+    intersection x y = x
+    top = AbsState.Top
+    bottom = AbsState.Bottom
--- While-ADA/test/AbsStateTest.hs
+++ While-ADA/test/AbsStateTest.hs
@@ -9,18 +9,18 @@ module AbsStateTest (tests) where
 
     testAlter  = testCase "test alter 1" (assertEqual "" expected result)
         where 
-            expected = [("x",21),("y",30),("z",11)]
-            result = alter [("x",18),("y",30),("z",11)] "x" 21
+            expected = (S[("x",21),("y",30),("z",11)])
+            result = alter (S[("x",18),("y",30),("z",11)]) "x" 21
 
     testAlter1  = testCase "test alter 2" (assertEqual "" expected result)
         where 
-            expected = [("x",18),("y",30),("z",11),("j",21)]
-            result = alter [("x",18),("y",30),("z",11)] "j" 21
+            expected = (S[("x",18),("y",30),("z",11),("j",21)])
+            result = alter (S[("x",18),("y",30),("z",11)]) "j" 21
 
     testAlter2  = testCase "test alter 3" (assertEqual "" expected result)
         where 
-            expected = [("x",21),("x",30),("z",11)]
-            result = alter [("x",18),("x",30),("z",11)] "x" 21
+            expected = (S[("x",21),("x",30),("z",11)])
+            result = alter (S[("x",18),("x",30),("z",11)]) "x" 21
     
 
     instance UndefSup Int where 
@@ -30,12 +30,12 @@ module AbsStateTest (tests) where
     testLookup  = testCase "test lookup 1" (assertEqual "" expected result)
         where 
             expected = 18 :: Int
-            result = lookUp [("x",18),("y",30),("z",11)] "x" ::Int
+            result = lookUp (S[("x",18),("y",30),("z",11)]) "x" ::Int
 
     testLookup1  = testCase "test lookup 2" (assertEqual "" expected result)
         where 
             expected = 0 :: Int
-            result = lookUp [("x",18),("y",30),("z",11)] "j" ::Int
+            result = lookUp (S[("x",18),("y",30),("z",11)]) "j" ::Int
 
 
     
