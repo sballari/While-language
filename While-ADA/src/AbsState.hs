24acd1dd1ce5e521bc1caefe4371f6342e47c85f
sballari <ballarinsimone@gmail.com>
2018-12-29 16:52:49 +0100

    added component wise op

--- While-ADA/src/AbsState.hs
+++ While-ADA/src/AbsState.hs
@@ -4,52 +4,50 @@ module AbsState where
 
   type VarName = String
 
-  data AbsState a = S [(VarName, a)] | Top | Bottom deriving (Show, Eq)
+  data AbsState a = S [(VarName, a)] | Bottom deriving (Show, Eq)
   -- a should be the abstract domain
   -- haskell doesn't allow type constraint in data type
 
 
-  
-  -- instance Functor AbsState where
-  --   -- fmap :: (a->b) -> AbsState a -> AbsState b
-  --   fmap f as = case as of 
-  --                 S xs -> S [(n,f v)|(n,v)<-xs] 
-  --                 AbsState.Top -> AbsState.Top
-  --                 AbsState.Bottom -> AbsState.Bottom
-  -- instance Applicative AbsState where
-  --   -- pure :: a -> AbsState a
-  --   pure a = S ["", a]
-  --   -- <*> :: AbsState (a->b) -> AbsState a -> AbsState b
-  --   f <*> a = 
-
-
   alter :: AbsState a -> String -> a -> AbsState a
   alter AbsState.Bottom name v = AbsState.Bottom
-  alter AbsState.Top name v = AbsState.Top
   alter (S[]) name v = S [(name, v)]
   alter (S(x:xs)) name v = if (fst x)==name then S ((name,v):xs) 
                             else 
                               case (alter (S xs) name v) of 
                                 S xs -> S(x:xs)
-                                AbsState.Top -> AbsState.Top
                                 AbsState.Bottom -> AbsState.Bottom
 
                             
                           
-  lookUp :: (UndefSup a) => AbsState a -> String -> a  
+  lookUp :: UndefSup a => AbsState a -> String -> a  
   lookUp AbsState.Bottom name = undef
-  lookUp AbsState.Top name = undef 
-  lookUp (S xs) name = case lookup name xs of
-                      Nothing -> undef
-                      Just x -> x 
+  lookUp (S xs) name = case (lookup name xs) of
+                          Nothing -> undef
+                          Just x -> x 
 
-  class UndefSup a where
-    undef :: a 
   
   -- component wise extensions
-  instance Eq a => AbsDomain (AbsState a) where 
-    (<=) x y = True
-    union x y = x
-    intersection x y = x
-    top = AbsState.Top
-    bottom = AbsState.Bottom
+  (<=) :: (AbsDomain a) => AbsState a -> AbsState a -> Bool  
+  (<=) AbsState.Bottom _  = True
+  (<=) (S xs) (S ys) = (xs == ys)  
+  (<=) _ _ = False
+
+  union :: (AbsDomain a) => AbsState a -> AbsState a -> AbsState a
+  union x AbsState.Bottom = x
+  union AbsState.Bottom y = y  
+  union (S xs) (S ys) = S (Data.List.union xs ys)
+
+  intersection :: (AbsDomain a) => AbsState a -> AbsState a -> AbsState a
+  intersection _ AbsState.Bottom = AbsState.Bottom
+  intersection AbsState.Bottom _ = AbsState.Bottom
+  intersection (S xs) (S ys) = let i = intersect xs ys in 
+                                if (findEl bottom i) then AbsState.Bottom
+                                else (S i) 
+                             
+  --utility function
+  findEl :: (AbsDomain a) => a -> [(b,a)] -> Bool 
+  findEl el xs = foldr (\x r-> (((snd x) == el) || r) ) False xs
+    
+  class UndefSup a where
+    undef :: a 
