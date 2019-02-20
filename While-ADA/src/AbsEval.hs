3eea434985d3166733aa90439f87f254e67dcec2
sballari <ballarinsimone@gmail.com>
2019-02-20 10:35:25 +0100

    add E[stm] abs sem of expr in a  non relational domain

---
+++ While-ADA/src/AbsEval.hs
@@ -0,0 +1,16 @@
+module AbsEval where
+    import WhileStructures
+    import AbsDomain
+    import AbsState
+
+    --abstract semantic of expressions in a non-relational domain
+    exprE :: (AbsDomain a) => AExpr -> AbsState a -> a
+    exprE (Var x) s = lookUp s x 
+    exprE (Num n) _ = soundC (Num n)
+    exprE (Range x y) _ = soundRange (Range x y)
+    exprE (Minus e) s = absMinus (exprE e s)
+    exprE (Sum e1 e2) s = absSum (exprE e1 s) (exprE e2 s)
+    exprE (Mul e1 e2) s = absMul (exprE e1 s) (exprE e2 s)
+    exprE (Div e1 e2) s = absDiv (exprE e1 s) (exprE e2 s)
+
+
