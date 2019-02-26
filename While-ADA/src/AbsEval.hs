f4863535a104aaf8be0df15d08a415d46d8f7e69
sballari <ballarinsimone@gmail.com>
2019-02-26 17:40:52 +0100

    AbsSem aggiunta classe che contiene le funzioni di valutazione semantica astratte comuni

--- While-ADA/src/AbsEval.hs
+++ While-ADA/src/AbsEval.hs
@@ -4,28 +4,30 @@ module AbsEval where
     import AbsState as AS
 
     --abstract semantic of expressions in a non-relational domain
-    exprE :: (AbsDomain a) => AExpr -> AbsState a -> a
-    exprE (Var x) s = lookUp s x 
-    exprE (Num n) _ = soundC (Num n)
-    exprE (Range x y) _ = soundRange (Range x y)
-    exprE (Minus e) s = absMinus (exprE e s)
-    exprE (Sum e1 e2) s = absSum (exprE e1 s) (exprE e2 s)
-    exprE (Mul e1 e2) s = absMul (exprE e1 s) (exprE e2 s)
-    exprE (Div e1 e2) s = absDiv (exprE e1 s) (exprE e2 s)
 
-    semS :: (AbsDomain a) => Stm -> AbsState a -> AbsState a
-    semS (Assign var e) s      
-                    | s == Bottom           = Bottom
-                    | (exprE e s) == bottom = Bottom
-                    | otherwise             = alter s var (exprE e s)
+    class (AbsDomain a) => AbsEval a where     
+        exprE ::  AExpr -> AbsState a -> a
+        exprE (Var x) s = lookUp s x 
+        exprE (Num n) _ = soundC (Num n)
+        exprE (Range x y) _ = soundRange (Range x y)
+        exprE (Minus e) s = absMinus (exprE e s)
+        exprE (Sum e1 e2) s = absSum (exprE e1 s) (exprE e2 s)
+        exprE (Mul e1 e2) s = absMul (exprE e1 s) (exprE e2 s)
+        exprE (Div e1 e2) s = absDiv (exprE e1 s) (exprE e2 s)
 
-    condC :: (AbsDomain a) => BExpr -> AbsState a -> AbsState a 
-    --fina ultra grossa p. 54
-    condC (WTrue) s = s
-    condC (WFalse) s = Bottom
-    condC (And c1 c2) s = AS.meet (condC c1 s) (condC c2 s)
-    condC (Or c1 c2) s =  AS.join (condC c1 s) (condC c2 s)
-    condC _ s = s
+        semS ::  Stm -> AbsState a -> AbsState a
+        semS (Assign var e) s      
+                        | s == Bottom           = Bottom
+                        | (exprE e s) == bottom = Bottom
+                        | otherwise             = alter s var (exprE e s)
+
+        condC ::  BExpr -> AbsState a -> AbsState a 
+        --fina ultra grossa p. 54
+        condC (WTrue) s = s
+        condC (WFalse) s = Bottom
+        condC (And c1 c2) s = AS.meet (condC c1 s) (condC c2 s)
+        condC (Or c1 c2) s =  AS.join (condC c1 s) (condC c2 s)
+        condC _ s = s
 
 
     
