a1111b6df9bdc3a9fb9919649447685dd3ea4601
sballari <ballarinsimone@gmail.com>
2019-03-02 18:07:49 +0100

    modularizzata cfg differenziata debug cfg e vero cfg

--- While-ADA/src/CFG.hs
+++ While-ADA/src/CFG.hs
@@ -5,62 +5,78 @@ module CFG where
     import AbsState as AS
 
 
-    newtype Label = L Int deriving Show
+    newtype Label = L Int deriving (Show, Eq)
     type CGraph a = [(Label,AbsState a -> AbsState a, Label)]
+    type Graph a = [(Label, a, Label)]
+
 
     freshLabel::ST (Label)
     freshLabel = ST(\s -> (L s,s+1))
     takeLabel::ST (Label)
     takeLabel = ST(\s -> (L s,s))
 
-    cfg ::  AbsDomain a => Stm -> ST (CGraph a)
-    cfg (Assign v e) = 
+    createCFG :: AbsDomain a => Stm -> ST (CGraph a)
+    createCFG s = cfg s convertS convertB
+
+    debugCFG :: Stm -> ST(Graph String)
+    debugCFG s = cfg s debugS debugB
+
+    cfg ::  Stm -> (Stm -> b) -> (BExpr -> b) -> ST (Graph b)
+    cfg (Assign v e) f g= 
         do
             l1 <- freshLabel 
             l2 <- takeLabel
-            return [(l1,semSG (Assign v e), l2)]
-    cfg (Assert c) = 
+            return [(l1,f (Assign v e), l2)] --semSG
+    cfg (Assert c) f g = 
         do
             l1 <- freshLabel
             l2 <- takeLabel
-            return [(l1,condCG c, l2)]
-    cfg (Skip) = 
+            return [(l1,g c, l2)] -- condCG
+    cfg (Skip) f g = 
         do  
             l1 <- freshLabel
             l2 <- takeLabel
-            return [(l1,id,l2)]
+            return [(l1,f Skip,l2)] --id
 
 
-    cfg (Comp s1 s2) =
+    cfg (Comp s1 s2) f g =
         do 
-            g1 <- cfg s1
-            g2 <- cfg s2
+            g1 <- cfg s1 f g
+            g2 <- cfg s2 f g
             return (g1++g2)
 
-    cfg (Cond c s1 s2) = 
+    cfg (Cond c s1 s2) f g = 
         do
             l1 <- freshLabel
             l2 <- takeLabel
-            g1 <- cfg s1 -- l2 -> l3
+            g1 <- cfg s1 f g-- l2 -> l3
             l3 <- freshLabel
             l4 <- takeLabel
-            g2 <- cfg s2 -- l4 ->l5
+            g2 <- cfg s2 f g-- l4 ->l5
             l5 <- freshLabel 
             l6 <- takeLabel --join
-            return ([(l1,condCG c,l2), (l1,condCG (Neg c), l4), (l3,id,l6), (l5,id,l6)]++g1++g2)
+            return ([(l1,g c,l2), (l1,g (Neg c), l4), (l3,f Skip,l6), (l5,f Skip,l6)]++g1++g2)
     
-    cfg (While c s) = 
+    cfg (While c s) f g = 
         do
             l1 <- freshLabel
             l2 <-freshLabel
             l3 <-takeLabel 
-            g <- cfg s -- l3 -> l4
+            g1 <- cfg s f g-- l3 -> l4
             l4 <- freshLabel
             l5 <- takeLabel
-            return ([(l1,id,l2), (l2, condCG c, l3), (l2,condCG (Neg c), l5), (l4,id,l2)]++g)
+            return ([(l1,f Skip,l2), (l2, g c, l3), (l2,g (Neg c), l5), (l4,f Skip,l2)]++g1)
            
+    convertS :: AbsDomain a => Stm -> AbsState a -> AbsState a
+    convertS Skip = id
+    convertS (Assign x y) = semSG (Assign x y)
+    convertB :: AbsDomain a => BExpr -> AbsState a -> AbsState a
+    convertB x = condCG x
             
-            
+    debugS :: Stm -> String
+    debugS s = show s
+    debugB :: BExpr -> String 
+    debugB c = show c
 
     
 
