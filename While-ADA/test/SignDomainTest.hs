9fcf240418e53cf3ceef08ddf1e9b53618b231b9
sballari <ballarinsimone@gmail.com>
2019-04-01 17:39:05 +0200

    test per dom segni e signcond risultati non buoni

--- While-ADA/test/SignDomainTest.hs
+++ While-ADA/test/SignDomainTest.hs
@@ -7,9 +7,11 @@ module SignDomainTest  where
     import Test.Tasty
     import Test.Tasty.HUnit
     import AbsEval
+    import AbsDenSem
+    
     
 
-    tests = [ r1,r2]
+    tests = [ r1,r2,a1,a2]
 
     --relazione d'ordine
     r1 = testCase "<=" (assertEqual "" expected result)
@@ -54,3 +56,17 @@ module SignDomainTest  where
         where 
             expected = S[("Q",MoreEqZero),("R",MoreEqZero),("B",MoreEqZero)]
             result = signCondC (MoreEq (Var "R") (Var "B")) (S[("Q",MoreEqZero),("R",SignTop),("B",MoreEqZero)])
+
+    a1 = testCase "[sign dom] if R>=B then R=R-B else skip" (assertEqual "" expected result)
+        where 
+            expected = S[("Q",MoreEqZero),("R",MoreEqZero),("B",SignTop)]
+            testprog = Cond (MoreEq (Var "R") (Var "B")) (Assign ("R") (Sum (Var "R") (Minus(Var "B")))) Skip
+            initState = S[("Q",MoreEqZero),("R",SignTop),("B",SignTop)]
+            result = semS signCondC testprog initState
+            
+    a2 = testCase "[sign dom] if R<=B then B=B-R else skip" (assertEqual "" expected result)
+        where 
+            expected = S[("Q",MoreEqZero),("R",MoreEqZero),("B",SignTop)]
+            testprog = Cond (LessEq (Var "R") (Var "B")) (Assign ("B") (Sum (Var "B") (Minus(Var "R")))) Skip
+            initState = S[("Q",MoreEqZero),("R",SignTop),("B",SignTop)]
+            result = semS signCondC testprog initState
