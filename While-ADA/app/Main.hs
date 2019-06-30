4208b67d9e9b5e7163f91993bd9d58a8b040c03d
sballari <ballarinsimone@gmail.com>
2019-06-30 14:17:21 +0200

    titolo

--- While-ADA/app/Main.hs
+++ While-ADA/app/Main.hs
@@ -18,10 +18,12 @@ module Main where
 
     main :: IO() 
     main = do 
+            
+            putStrLn titleStr 
             setSGR [ SetConsoleIntensity BoldIntensity
                 , SetColor Foreground Vivid Red
                 -- , SetColor Background Dull Green
-                ]           
+                ]        
             putStrLn "Benvenuti in While-ADA v3.1!"
             setSGR [ Reset ]
             
@@ -53,4 +55,4 @@ module Main where
             putStrLn "Arrivederci"
 
 
-    
+    titleStr = "\n888       888 888      d8b 888                d8888 8888888b.        d8888\n888   o   888 888      Y8P 888               d88888 888  'Y88b      d88888\n888  d8b  888 888          888              d88P888 888    888     d88P888\n888 d888b 888 88888b.  888 888  .d88b.     d88P 888 888    888    d88P 888\n888d88888b888 888 '88b 888 888 d8P  Y8b   d88P  888 888    888   d88P  888\n88888P Y88888 888  888 888 888 88888888  d88P   888 888    888  d88P   888\n8888P   Y8888 888  888 888 888 Y8b.     d8888888888 888  .d88P d8888888888\n888P     Y888 888  888 888 888  'Y8888 d88P     888 8888888P' d88P     888\n"
