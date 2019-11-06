9578f39fdffb48391e00e5c90b31833b318964bc
sballari <ballarinsimone@gmail.com>
2019-11-06 13:24:33 +0100

    gauss jordan works

--- While-ADA/src/karrDomain.hs
+++ While-ADA/src/karrDomain.hs
@@ -1,6 +1,6 @@
+--{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module KarrDomain where
     
-     
     data EQs = EQs (RowForm Double,[Double]) -- <M,X> 
                  | EQsBottom deriving (Show, Eq)
 
@@ -8,14 +8,10 @@ module KarrDomain where
     type ColForm a = [[a]] 
     
     coefs :: EQs -> RowForm Double
-    coefs (EQs (m,c)) = m 
+    coefs (EQs s)=fst s
     consts :: EQs -> [Double]
-    consts (EQs (m,c)) = c
+    consts (EQs s)=snd s
 
-    -- firstNZel :: (Eq a, Num a) => [a]->Maybe a
-    -- firstNZel ([]) = Nothing 
-    -- firstNZel (0:xs) = firstNZel xs
-    -- firstNZel (x:xs) = (Just x)
         
     firstNZRow :: (Eq a, Num a) => RowForm a -> Maybe (a,[a])
     -- nonzero element
@@ -39,19 +35,25 @@ module KarrDomain where
     restCol rs = fmap tail rs
 
     firstCol :: RowForm a ->[a]
-    firstCol = fmap head
+    firstCol ms = fmap head ms
 
     gaussJordanEl :: EQs -> EQs 
-    gaussJordanEl (EQs ([[]],[])) = EQs ([[]],[])
+
+    gaussJordanEl (EQs ([],[])) = EQs ([],[]) --maybe useless
+    gaussJordanEl (EQs (m:[],[c])) = 
+        case firstNZCol ([m]) of
+            Just (nz_el,nz_col) -> EQs ([fmap (/nz_el) m], [c/nz_el])
+            Nothing -> EQs ([m],[c])
+
     gaussJordanEl (EQs (m:ms,c:cs)) = 
-        case (firstNZCol ms) of
+        case (firstNZCol (m:ms)) of 
             Just (nz_el,nz_col) -> 
                 let 
-                    m' = fmap (/nz_el) m
-                    c' = c/nz_el
-                    el_coef = fmap (*(-1)) nz_col
+                    m' = fmap (/nz_el) m  --row queen
+                    c' = c/nz_el -- c queen
+                    el_coef = fmap (*(-1)) nz_col -- coeffs di eliminazione
                     ms' = zerofication ms m' el_coef
-                    [cs'] = transpose (zerofication (transpose [cs]) [c'] el_coef )
+                    [cs'] = transpose (zerofication (transpose [cs]) [c'] el_coef ) --actung!
                     --TODO: si potrebbe scrivere una zerofication ad hoc senza dover fare la trasporta
                     rc = gaussJordanEl (EQs (ms',cs') ) -- rec call
                 in EQs (m':(coefs rc), c':(consts rc))
@@ -61,12 +63,16 @@ module KarrDomain where
                     EQs (m:(coefs rc), c:(consts rc))
 
             
-    zerofication ::(Num a) => RowForm a -> [a] -> [a] -> RowForm a
+    zerofication ::(Num a,Show a) => RowForm a -> [a] -> [a] -> RowForm a
     -- sottomatrice da zeroficare
     -- riga regina 
     -- coefficiente di eliminazione 
     zerofication [] _ _ = []
-    zerofication ([[]]) _ _ = []
     zerofication (m:ms) qr (ec:ecs) = (zipWith (+) (fmap (*ec) qr) m) : (zerofication ms qr ecs)
 
-    
+    main  = 
+        let
+            m = [[0,1,2,1],[0,0,1,2]]
+            c = [3,3]
+        in print (gaussJordanEl (EQs (m,c)))
+        
--- While-ADA/test/GJTest.hs
+++ While-ADA/test/GJTest.hs
@@ -3,7 +3,7 @@ module GJTest where
     import Test.Tasty
     import Test.Tasty.HUnit
 
-    tests = [a0,a01,a1,a10,a2,a3,a4,a5,a5bis,a5ter,a6]
+    tests = [a0,a01,a1,a10,a11,a12,a2,a3,a4,a5,a5bis,a5ter,a5quad,a6,a7,a8]
 
     a0 = testCase "[GJtest] Transpose" (assertEqual "" expected result) 
         where 
@@ -15,9 +15,9 @@ module GJTest where
             expected = [[1,4],[2,5],[3,6]]
             result = rowM2colM ([[1,2,3],[4,5,6]])
 
-    a1 = testCase "[GJtest] [[1,2,3,2],[1,2,4,4],[0,1,2,1]], [2,5,3]" (assertEqual "" expected result)
+    a1 = testCase "[GJtest][a1] [[1,2,3,2],[1,2,4,4],[0,1,2,1]], [2,5,3]" (assertEqual "" expected result)
         where 
-            expected = EQs([[ 1,2,3,2],[0,1,2,1],[0,0,1,2]],[2,3,3])
+            expected = EQs([[ 1,2,3,2],[0,0,1,2],[0,1,0,-3]],[2,3,-3])
             result = gaussJordanEl (EQs (m,c))
             m = [[1,2,3,2],[1,2,4,4],[0,1,2,1]]
             c = [2,5,3]
@@ -28,6 +28,19 @@ module GJTest where
                 result = gaussJordanEl (EQs (m,c))
                 m = [[1,2,3,2]]
                 c = [2]
+    a11 = testCase "[GJtest] [[0,1,2,1],[0,0,1,2]], [3,3]" (assertEqual "" expected result)
+        where 
+            expected = EQs([[0,1,2,1],[0,0,1,2]],[3,3])
+            result = gaussJordanEl (EQs (m,c))
+            m = [[0,1,2,1],[0,0,1,2]]
+            c = [3,3]
+
+    a12 = testCase "[GJtest][a12] [[0,1,2,1]], [3]" (assertEqual "" expected result)
+        where 
+            expected = EQs([[0,0,1,2]],[3])
+            result = gaussJordanEl (EQs (m,c))
+            m = [[0,0,1,2]]
+            c = [3]
 
     a2 = testCase "[GJtest] zerofication [[1,2,3,2],[1,2,4,4],[0,1,2,1]], [2,5,3]" (assertEqual "" expected result)
         where 
@@ -65,14 +78,39 @@ module GJTest where
 
     a5ter = testCase "[GJtest] zerofication no row" (assertEqual "" expected result)
         where
-            expected = [3,3]
-            [result] = transpose (zerofication (transpose [cs]) [c'] el_coef )
+            expected = []
+            result = transpose (zerofication (transpose cs) [c'] el_coef )
             c' = 2
             cs = []
+            el_coef = []
+
+    a5quad = testCase "[GJtest] zerofication no row" (assertEqual "" expected result)
+        where
+            expected = []
+            result = zerofication ms m el_coef
+            m = [1,2,3]
+            ms = []
             el_coef = []
             
     a6 = testCase "[GJtest] firstNZCol [[1,2,3]]" (assertEqual "" expected result)
             where
                 expected = Just (2,[])
                 result = firstNZCol [[0,2,3]]
-                
+    a7 = testCase "[GJtest][a7] zerofication [[0,1,2,1],[0,0,1,2]],[3,3]" (assertEqual "" expected result)
+        where
+            expected = ([[0.0,0.0,1.0,2.0]],[3])
+            result = ((zerofication ms m el_coef),(cs'))
+            el_coef = fmap (*(-1)) nz_col
+            Just (nz_el,nz_col) = firstNZCol (m:ms)
+            (m:ms) = [[0.0,1.0,2.0,1.0],[0.0,0.0,1.0,2.0]]
+            (c:cs) = [3.0,3.0]
+            c' = c/nz_el
+            [cs']=transpose (zerofication (transpose [cs]) [c'] el_coef )
+
+    a8 = testCase "[GJtest][a8] find el coef [[0,0,1,2],[0,1,2,1]]" (assertEqual "" expected result)
+        where
+            expected = (1,[2],[-2])
+            result = (nz_el,nz_col,el_coef)
+            el_coef = fmap (*(-1)) nz_col
+            Just (nz_el,nz_col) = firstNZCol (m:ms)
+            (m:ms) = [[0,0,1,2],[0,1,2,1]]
