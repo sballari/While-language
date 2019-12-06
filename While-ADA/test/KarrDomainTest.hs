a1cdc102c6956d7dfecbb6a8bc2bb9fbd2da4e58
sballari <ballarinsimone@gmail.com>
2019-12-06 16:26:33 +0100

    add test on condC (very simple)

--- While-ADA/test/KarrDomainTest.hs
+++ While-ADA/test/KarrDomainTest.hs
@@ -1,10 +1,11 @@
 module KarrDomainTest where
     import KarrDomain as KD
     import AbsDomainR
+    import WhileStructures
     import Test.Tasty
     import Test.Tasty.HUnit
 
-    tests = [a0,a1]
+    tests = [a0,a1,c1,c2,c3]
 
     a0 = testCase "[KarrDomain Test][a0] join (ex5.5 p109)" (assertEqual "" expected result) 
         where
@@ -35,3 +36,25 @@ module KarrDomainTest where
             p1 = EQs ([[1,0],[0,1]],[1,10],o) -- programs s.t. x1 = 1 and x2 = 10
             p2 = EQs ([[1,0],[0,1]],[2,12],o) -- programs s.t. x1 = 2 and x2 = 12
             o = ["x","y","z"]
+    
+    c1 = testCase "[KarrDomain Test][c1] condC x+y = 3x -2" (assertEqual "" expected result) 
+        where
+            expected = EQs ([[1,0,0],[0,1,0],[0,0,1],[-2,1,0]],[1,10,2,-2],o)
+            result = condC bExpr sys
+            sys = EQs ([[1,0,0],[0,1,0],[0,0,1]],[1,10,2],o)
+            o = ["x","y","z"]
+            bExpr =  Eq (Sum (Var "x") (Var "y")) (Sum (Mul (Num 3) (Var "x")) (Minus (Num 2)) )
+    c2 = testCase "[KarrDomain Test][c2] condC 2*(x+y) = 3x -2" (assertEqual "" expected result) 
+        where
+            expected = EQs ([[1,0,0],[0,1,0],[0,0,1],[-1,2,0]],[1,10,2,-2],o)
+            result = condC bExpr sys
+            sys = EQs ([[1,0,0],[0,1,0],[0,0,1]],[1,10,2],o)
+            o = ["x","y","z"]
+            bExpr =  Eq (Mul (Num 2) ((Sum (Var "x") (Var "y")))) (Sum (Mul (Num 3) (Var "x")) (Minus (Num 2)) )
+    c3 = testCase "[KarrDomain Test][c2] condC z*(x+y) = 3x -2" (assertEqual "" expected result) 
+        where
+            expected = sys
+            result = condC bExpr sys
+            sys = EQs ([[1,0,0],[0,1,0],[0,0,1]],[1,10,2],o)
+            o = ["x","y","z"]
+            bExpr =  Eq (Mul (Var "z")(  (Sum (Var "x") (Var "y")))) (Sum (Mul (Num 3) (Var "x")) (Minus (Num 2)) )
